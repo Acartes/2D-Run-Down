69bda4f887207b544cafbb3f441f48781ef31461
Arthur Llorens <arthurllorens@hotmail.fr>
2016-10-20 23:04:07 +0200

    Suite de l'intégration du niveau de base
    -Supprimé les scenes persos : on est est pas là!
    -Ajouté scene de base (à finir d'abord)
    -Script "PlayerMove" plus propre
    -Ajouté saut dans le script "PlayerMove". On peut modifier la variable
    "jumpForce"
    -Le level design du niveau permet de tester le saut
    -Supprimé le sprite du nuage rouge
    -Ajouté à la place un nuage vert de particules. La taille de la box est
    cohérente par rapport au nuage.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,50 +3,73 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
 
+
 	public float maxSpeed;
 	public float moveForce;
+    public float jumpForce;
 
-	public float h;
+    public float h;
 	public float a;
 	public float b;
 
 	string Direction = null;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private Rigidbody2D body2D;
+    private RaycastHit2D groundDetect;
+
+    // Use this for initialization
+    void Start () {
+        body2D = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-		// Cache the horizontal input.
-		h = Input.GetAxis("Horizontal");
-
-		a = GetComponent<Rigidbody2D> ().velocity.x;
-		b = GetComponent<Rigidbody2D> ().velocity.y;
-
-		if (h > 0) {
-			Direction = "right";
-		}
-		if (h < 0) {
-			Direction = "left";
-		}
-
-
-		if (Direction == "right") {
-
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (10, b);
-		}
-
-		if (Direction == "left") {
-
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-10, b);
-		}
-		/*if(Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
-			
-			GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);*/
+        Movement();
+        PlayerJump();
 	}
+
+    void Movement()
+    {
+        // Cache the horizontal input.
+        h = Input.GetAxis("Horizontal");
+        a = body2D.velocity.x;
+        b = body2D.velocity.y;
+
+        if (h > 0)
+        {
+            Direction = "right";
+        }
+        if (h < 0)
+        {
+            Direction = "left";
+        }
+
+
+        if (Direction == "right")
+        {
+
+            body2D.velocity = new Vector2(10, b);
+        }
+
+        if (Direction == "left")
+        {
+
+            body2D.velocity = new Vector2(-10, b);
+        }
+        /*if(Mathf.Abs(body2D.velocity.x) > maxSpeed)
+			
+			body2D.velocity = new Vector2(Mathf.Sign(body2D.velocity.x) * maxSpeed, body2D.velocity.y);*/
+    }
+
+    void PlayerJump()
+    {
+        if (body2D.velocity.y == 0)
+            if (Input.GetButton("Jump"))
+            {
+                body2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+    }
 
 
 }
